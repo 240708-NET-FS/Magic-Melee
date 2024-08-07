5b0bea1168e930522cca2c203de47528a49eaef9
erichards831 <lizrich831@gmail.com>
2024-08-07 11:42:59 -0400

    fixing the timeline

--- frontend/src/Screens/Home.js
+++ frontend/src/Screens/Home.js
@@ -2,28 +2,19 @@ import React from 'react';
 import HomeStyles from "../Styles/HomeStyles.css"
 import NavBar from '../Components/NavBar';
 import CharacterCard from '../Components/HomeComps/CharacterCard';
-// import { useState } from 'react';
 
 function Home() {
 
-    // const [loading, setLoading] = useState(null);
-
-
-
-    //get users' cards and things
-
-    
 
     // map the cards
     return(
         <div className='home'>
+            <NavBar />
             {/* <h1>Home</h1> */}
             <div style={{padding: 75}}>
                 <div className='user-charactersBox'>
                     <h2>My Characters</h2>
                 </div>
-
-                
                 <div class="card-box">
                     <CharacterCard />
 
