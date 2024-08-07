e93f84dcbc097f53dac2b7900aa4785f608b17ce
erichards831 <lizrich831@gmail.com>
2024-08-07 11:37:43 -0400

    saving before unstashing

--- frontend/src/Screens/Home.js
+++ frontend/src/Screens/Home.js
@@ -1,11 +1,35 @@
 import React from 'react';
-
+import HomeStyles from "../Styles/HomeStyles.css"
+import NavBar from '../Components/NavBar';
+import CharacterCard from '../Components/HomeComps/CharacterCard';
+// import { useState } from 'react';
 
 function Home() {
 
+    // const [loading, setLoading] = useState(null);
+
+
+
+    //get users' cards and things
+
+    
+
+    // map the cards
     return(
-        <div>
-            <h1>Home</h1>
+        <div className='home'>
+            {/* <h1>Home</h1> */}
+            <div style={{padding: 75}}>
+                <div className='user-charactersBox'>
+                    <h2>My Characters</h2>
+                </div>
+
+                
+                <div class="card-box">
+                    <CharacterCard />
+
+                </div>
+            </div>
+           
         </div>
     )
 
