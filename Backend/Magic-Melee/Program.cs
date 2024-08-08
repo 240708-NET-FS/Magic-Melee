f4b373d300df05427daebc6f65abc437a4e7521e
Francie <40801017+MulliganF02@users.noreply.github.com>
2024-08-08 11:16:35 -0400

    Program.cs updates

--- Backend/Magic-Melee/Program.cs
+++ Backend/Magic-Melee/Program.cs
@@ -143,8 +143,8 @@ app.Run();
                                             ValidateAudience = true,
                                             ValidateLifetime = true,
                                             ValidateIssuerSigningKey = true,
-                                            ValidIssuer = configuration["Jwt:Issuer"],
-                                            ValidAudience = configuration["Jwt:Audience"],
-                                            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]))
+                                            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                                            ValidAudience = builder.Configuration["Jwt:Audience"],
+                                            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
                                      };
                                  });
